d558108dd9bc7bb5fadabcc1769d298ecea8ef1c
Pradeep Pandit <pp8379951@gmail.com>
2026-04-13 23:50:33 +0530

    Update Done

--- Paste-App/src/components/Navbar.jsx
+++ Paste-App/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row gap-4 bg-gradient-to-r from-[#ff5733] to-[#33cfff] font-semibold text-white p-4 place-content-evenly'>
+    <div className='flex flex-row gap-4 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-pulse font-semibold text-white p-4 place-content-evenly'>
       <NavLink to="/">
         Home
       </NavLink>
